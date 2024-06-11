4a9f7c95c35853d64a6f01e76228b99673819b7b
AmmaVinay <ammavinay917@gmail.com>
2024-06-11 23:28:18 +0530

    contact, login & signup -- connection

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,16 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 function Contact() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [description, setDescription] = useState('')
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/contact', { name, email, description })
+      setMessage(response.data.message)
+      setName('');
+      setEmail('');
+      setDescription('');
+      setMessage('')
+    } catch {
+      setMessage('An error occurred while submitting the form*')
+    }
+  }
   return (
     <div className="min-h-screen bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h1 className="text-3xl font-bold text-gray-800 mb-6 text-center">Contact Us</h1>
-        <form action="https://fabform.io/f/{form-id}" method="post">
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">
               Name
             </label>
-            <input
+            <input value={name} onChange={(e) => setName(e.target.value)}
               type="text"
               id="name"
               name="name"
@@ -23,7 +41,7 @@ function Contact() {
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
               Email
             </label>
-            <input
+            <input value={email} onChange={(e) => setEmail(e.target.value)}
               type="email"
               id="email"
               name="email"
@@ -36,7 +54,7 @@ function Contact() {
             <label htmlFor="message" className="block text-sm font-medium text-gray-700">
               Message
             </label>
-            <textarea
+            <textarea value={description} onChange={(e) => setDescription(e.target.value)}
               id="message"
               name="message"
               className="border-2 border-gray-300 p-2 rounded-lg w-full h-32 resize-none focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
@@ -51,6 +69,7 @@ function Contact() {
             Send
           </button>
         </form>
+        {message && <p className=" text-red-600">{message}</p>}
       </div>
     </div>
   );
